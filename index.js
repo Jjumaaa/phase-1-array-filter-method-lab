033ccdaf37f97f8c16e9c270dea43bc725e6e780
Juma <gerald.juma@student.moringaschool.com>
2025-03-20 02:23:50 +0300

    everything is returned well

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const users = [
+    { name: "Bobby", hometown: "Pittsburgh" },
+    { name: "salim", hometown: "Unknown" },
+    { name: "Mikey", hometown: "Unknown" },
+    { name: "Peter", hometown: "Unknown" },
+    { name: "Bobby", hometown: "Tampa Bay" },
+    { name: "Salim", hometown: "Unknown" },
+];
+
+function findMatching(array, string) {
+  let newArray = array.filter((input) =>
+    input.toLowerCase().startsWith(string.toLowerCase())
+  );
+  return newArray;
+}
+
+function fuzzyMatch(array, string) {
+  let newArray = array.filter((input) => input.startsWith(string));
+  return newArray;
+}
+
+function matchName(array, string) {
+  let newArray = array.filter((input) => input.name === string);
+  return newArray;
+}
